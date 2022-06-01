31f3be304ed535eca7fbcc89121b6c0db089f4c3
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-06-01 23:58:16 +0430

    import components to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="text-white overflow-hidden">
+      <Header />
+      <Hero />
+    </div>
+  );
 }
 
 export default App;
