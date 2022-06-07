cc147fe684cdb42f40824162d68121d54b58e144
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-06-07 21:30:27 +0430

    Create search section right side

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,7 +6,7 @@ function Search() {
      flex items-center justify-between rounded-b-[5rem]"
     >
       {/* left side */}
-      <section className="left ">
+      <section className="left flex-1">
         <img
           src={require("../image/backgraphics.png")}
           alt=""
@@ -34,7 +34,27 @@ function Search() {
         />
       </section>
       {/* right side */}
-      <section className="right"></section>
+      <section className="right flex items-start flex-col justify-start flex-1 h-[100%] pt-[9rem]">
+        {/* Search bar */}
+        <section className="searchbar flex justify-start w-[100%]">
+          <input
+            type="text"
+            placeholder="Enter the keyword or URL"
+            className="flex-[19] ml-[1rem] outline-none bg-[#020917] rounded-xl p-3 h-[3rem]"
+          />
+          {/* search icon */}
+          <section
+            className="searchIcon flex flex-1 items-center rounded-xl ml-4
+          bg-gradient-to-bl from-[#f3071d] to-[#e600ff] p-4 h-[3rem]"
+          >
+            <img
+              src={require("../image/search.png")}
+              alt=""
+              className="w-[1.5rem]"
+            />
+          </section>
+        </section>
+      </section>
     </section>
   );
 }
