4dceafbc61b55c04448c73bdc4c31460af3576a8
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-06-08 11:35:00 +0430

    Create footer section

---
+++ src/components/Footer.js
@@ -0,0 +1,40 @@
+import React from "react";
+import CenterMenu from "./CenterMenu";
+import { Facebook, Twitter, YouTube, LinkedIn } from "@material-ui/icons";
+
+function Footer() {
+  const socialStyle = "rounded-full border-2 border-white p-2 mr-[5rem]";
+
+  return (
+    <section
+      className="footer flex flex-col items-center justify-start px-[5rem] pt-[18rem] mt-[-10rem]
+       h-[45rem] relative z-[-1] bg-[#081730]"
+    >
+      <CenterMenu />
+      {/* Social icons */}
+      <section className="flex w-[100%] justify-center mt-14">
+        <section className={`facebook ${socialStyle}`}>
+          <Facebook />
+        </section>
+        <section className={`twitter ${socialStyle}`}>
+          <Twitter />
+        </section>
+        <section className={`youtube ${socialStyle}`}>
+          <YouTube />
+        </section>
+        <section className={`linkedin ${socialStyle}`}>
+          <LinkedIn />
+        </section>
+      </section>
+      {/* details */}
+      <section className="detils flex justify-center">
+        <span className="text-[1rem] text-gray-400 text-center mt-[4rem] px-[15rem]">
+          Duis feugiat congue metus ultrices vulputate nibh viverra eget.
+          Vestibulum ullamcorper volutpat <br /> varius
+        </span>
+      </section>
+    </section>
+  );
+}
+
+export default Footer;
